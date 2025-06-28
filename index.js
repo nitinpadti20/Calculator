880eef6bc79febc57cb71a6d53e8d9349ffb5aa3
nitinpadti20 <nitinpadti75@gmail.com>
2025-06-28 21:33:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//calculator program
+
+const display=document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    display.value = eval(display.value);
+}
